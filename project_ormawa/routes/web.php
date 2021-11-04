30cb225215c5ca83c7d207143590b28280f88f7f
zakariazikri <zakariazikri@apps.ipb.ac.id>
2021-11-04 16:05:42 +0700

    initial commit

--- project_ormawa/routes/web.php
+++ project_ormawa/routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/public/index', function (){
-    return view('/public.index');
-});
+// Route::get('/public/index', function (){
+//     return view('/public.index');
+// });
